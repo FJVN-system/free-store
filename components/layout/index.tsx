7aa1b85dd74b930fae940ed41ce089c01aea1dd7
KyungHwanKim-devs <kkhdevs@gmail.com>
2023-02-26 16:47:18 +0900

    a

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,11 @@
+import styled from "@emotion/styled";
 import React from "react";
 import Header from "../header";
+
+const LayoutContainer = styled.div`
+  display: flex;
+  flex: 1;
+`;
 
 export default function Layout({ children }: any) {
   return (
